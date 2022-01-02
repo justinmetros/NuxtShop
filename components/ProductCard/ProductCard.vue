85b5e7e02c1b6813dab9bb26ff93611d4bafff7b
Justin Metros <jmetros@universalstandard.net>
2022-01-02 00:55:36 -0500

    Scaffold out ProductCard

---
+++ components/ProductCard/ProductCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-if="product">
+    {{ props.product.title }}
+  </div>
+</template>
+
+<script setup lang="ts">
+interface ProductCard {
+  title: string;
+}
+
+const props = defineProps<{
+  product: ProductCard;
+}>();
+</script>
