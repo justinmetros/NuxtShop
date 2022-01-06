0ce116d60ed9aa4a8a7d608fe61008447a6ed29d
Justin Metros <jmetros@universalstandard.net>
2022-01-05 21:28:04 -0500

    Adds money format to shop query

--- stores/shop.ts
+++ stores/shop.ts
@@ -5,6 +5,7 @@ import { shopQuery } from "~~/apollo/queries/shop";
 export const useShopStore = defineStore("shop", {
   state: () => {
     return {
+      moneyFormat: "$",
       loading: false,
       description: "",
     };
@@ -21,12 +22,16 @@ export const useShopStore = defineStore("shop", {
         if (!data.shop) {
           throw "getShopData: no response";
         }
+        this.setMoneyFormat(data.shop?.moneyFormat ?? "$");
         this.setShopDescription(data.shop?.description ?? "");
       } catch (e) {
         return e;
       } finally {
         this.loading = false;
       }
+    },
+    setMoneyFormat(value: string) {
+      this.moneyFormat = value;
     },
     setShopDescription(value: string) {
       this.description = value;
