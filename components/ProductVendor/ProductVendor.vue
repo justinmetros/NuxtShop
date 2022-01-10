805c4d5122e602edac965b1830a4be17372de069
Justin Metros <jmetros@universalstandard.net>
2022-01-09 21:52:54 -0500

    Add ProductVendor component

---
+++ components/ProductVendor/ProductVendor.vue
@@ -0,0 +1,15 @@
+<template>
+  <component v-if="vendor" :is="element">
+    {{ vendor }}
+  </component>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  tag?: string;
+  vendor: string;
+}>();
+
+const { tag, vendor } = toRefs(props);
+const element = tag.value || "div";
+</script>
