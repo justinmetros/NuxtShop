a2decef37e36bd6ae863b210263e7455997f2d1d
Justin Metros <jmetros@universalstandard.net>
2022-01-14 03:52:41 -0500

    Add actions, state and getters to cart store

--- stores/cart.ts
+++ stores/cart.ts
@@ -1,8 +1,22 @@
 import { defineStore } from "pinia";
+import { useApolloClient } from "@vue/apollo-composable";
+import { cartCreate } from "~/apollo/mutations/cartCreate";
+import { cartLinesAdd } from "~/apollo/mutations/cartLinesAdd";
+import { checkoutUrl } from "~/apollo/queries/checkoutUrl";
+
+// TO-DO: provide apolloClient to pinia as plugin
+// https://pinia.vuejs.org/core-concepts/plugins.html#introduction
 
 export const useCartStore = defineStore("cart", {
   state: () => {
     return {
+      cart: {
+        checkoutUrl: null,
+        id: null,
+        lines: {
+          edges: [],
+        },
+      },
       loading: false,
       cartOpen: false,
     };
@@ -15,5 +29,84 @@ export const useCartStore = defineStore("cart", {
         this.cartOpen = !this.cartOpen;
       }
     },
+    async cartCreate() {
+      try {
+        this.loading = true;
+        const { resolveClient } = useApolloClient();
+        const apolloClient = resolveClient();
+        const { data } = await apolloClient.mutate({
+          mutation: cartCreate,
+        });
+        if (!data.cartCreate.cart.id) {
+          throw "cartCreate: error";
+        }
+        this.cart.id = data.cartCreate.cart.id;
+        this.cart.checkoutUrl = data.cartCreate.cart.checkoutUrl;
+      } catch (e) {
+        return e;
+      } finally {
+        this.loading = false;
+      }
+    },
+    async getCheckoutUrl() {
+      if (this.checkoutUrl) {
+        return this.checkoutUrl;
+      }
+      if (!this.cart.id) {
+        await this.cartCreate();
+      }
+      try {
+        this.loading = true;
+        const { resolveClient } = useApolloClient();
+        const apolloClient = resolveClient();
+        const { data } = await apolloClient.query({
+          query: checkoutUrl,
+          variables: {
+            cartId: this.cart.id,
+          },
+        });
+        if (!data.checkoutUrl.cart.checkoutUrl) {
+          throw "checkoutUrl: error";
+        }
+        this.cart.checkoutUrl = data.checkoutUrl.cart.checkoutUrl;
+        return this.cart.checkoutUrl;
+      } catch (e) {
+        return e;
+      } finally {
+        this.loading = false;
+      }
+    },
+    async cartLinesAdd(merchandiseId: string) {
+      try {
+        if (!this.cart.id) {
+          await this.cartCreate();
+        }
+        const { resolveClient } = useApolloClient();
+        const apolloClient = resolveClient();
+        const { data } = await apolloClient.mutate({
+          mutation: cartLinesAdd,
+          variables: {
+            lines: [
+              {
+                merchandiseId,
+              },
+            ],
+            cartId: this.cart.id,
+          },
+        });
+        if (!data.cartLinesAdd) {
+          throw "cartCreate: error";
+        }
+        this.cart = data.cartLinesAdd.cart;
+        this.cartOpen = true;
+      } catch (e) {
+        return e;
+      } finally {
+        this.loading = false;
+      }
+    },
+  },
+  getters: {
+    lineItems: (state) => state.cart.lines.edges,
   },
 });
