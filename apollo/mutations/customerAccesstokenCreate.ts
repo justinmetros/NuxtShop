1c35e50e1e7af57762f217fdf6f66a586f6fc54b
Justin Metros <jmetros@universalstandard.net>
2022-01-14 00:19:54 -0500

    Add customerAccessTokenCreate mutation

---
+++ apollo/mutations/customerAccesstokenCreate.ts
@@ -0,0 +1,17 @@
+import { gql } from "graphql-tag";
+
+export const customerAccessTokenCreate = gql`
+  mutation customerAccessTokenCreate($input: CustomerAccessTokenCreateInput!) {
+    customerAccessTokenCreate(input: $input) {
+      customerAccessToken {
+        accessToken
+        expiresAt
+      }
+      customerUserErrors {
+        code
+        field
+        message
+      }
+    }
+  }
+`;
