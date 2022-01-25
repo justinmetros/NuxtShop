cc3b22b38c709d56516cab53d9c0cba43b5c5b7a
Justin Metros <jmetros@universalstandard.net>
2022-01-25 03:39:32 -0500

    Update ProductGrid

--- components/ProductGrid/ProductGrid.vue
+++ components/ProductGrid/ProductGrid.vue
@@ -1,7 +1,5 @@
 <template>
-  <section
-    class="grid grid-cols-2 gap-4 mx-4 md:mx-auto md:max-w-6xl md:grid-cols-4"
-  >
+  <section class="container grid grid-cols-2 gap-4 mx-auto md:grid-cols-4">
     <slot></slot>
   </section>
 </template>
