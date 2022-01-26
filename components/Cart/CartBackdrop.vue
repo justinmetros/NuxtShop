d6ab74d03e229b280f2df8cf5aba3993cea7d640
Justin Metros <jmetros@universalstandard.net>
2022-01-26 15:19:15 -0500

    Add CartBackdrop

---
+++ components/Cart/CartBackdrop.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="fixed top-0 bottom-0 left-0 right-0 bg-black bg-opacity-20"
+    @click="closeCart"
+    @keyup.enter="closeCart"
+  >
+    &nbsp;
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useCartStore } from "~/stores/cart";
+
+const cartStore = useCartStore();
+const closeCart = () => cartStore.toggleCart(false);
+</script>
